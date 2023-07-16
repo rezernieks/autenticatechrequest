a0d4687494d1c5742cf78e251f413745edb5ed11
rezernieks <59909616+rezernieks@users.noreply.github.com>
2023-07-16 23:10:09 +0300

    frontend and cors

--- autentica-frontend/src/app/app.component.ts
+++ autentica-frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TechRequest } from './techrequest';
+import { TechRequestService } from './techrequest.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'autentica-frontend';
+export class AppComponent implements OnInit{
+  public techrequests: TechRequest[] = [];
+
+  constructor(private techrequestService: TechRequestService){}
+
+  ngOnInit(): void {
+    this.getTechRequests();
+  }
+
+  public getTechRequests(): void {
+    this.techrequestService.getTechRequests().subscribe(
+      (response: TechRequest[]) => {
+        this.techrequests = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
