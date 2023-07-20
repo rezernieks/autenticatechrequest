b407d6c02a9e42d575200e25b49f197b7e31d427
rezernieks <59909616+rezernieks@users.noreply.github.com>
2023-07-20 18:01:20 +0300

    frontend completed

--- autentica-frontend/src/app/app.component.ts
+++ autentica-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TechRequest } from './techrequest';
 import { TechRequestService } from './techrequest.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,9 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class AppComponent implements OnInit{
   public techrequests: TechRequest[] = [];
 
-  constructor(private techrequestService: TechRequestService){}
+  constructor(
+    private techrequestService: TechRequestService, 
+  ) {}
 
   ngOnInit(): void {
     this.getTechRequests();
@@ -21,10 +24,70 @@ export class AppComponent implements OnInit{
     this.techrequestService.getTechRequests().subscribe(
       (response: TechRequest[]) => {
         this.techrequests = response;
+        console.log(response);
       },
       (error: HttpErrorResponse) => {
-        alert(error.message);
+        console.log(error.message);
+      }
+    );
+    
+  }
+
+  public acceptTechRequest(techrequestId: number): void {
+    this.techrequestService.acceptTechRequests(techrequestId).subscribe(
+      (response: string) => {
+        console.log(response);
+        this.getTechRequests(); // refreshes the techrequests data
+      },
+      (error: HttpErrorResponse) => {
+        this.getTechRequests();
+        console.log(error.message);
       }
     );
   }
+
+  public declineTechRequest(techrequestId: number): void {
+    this.techrequestService.declineTechRequests(techrequestId).subscribe(
+      (response: string) => {
+        console.log(response);
+        this.getTechRequests(); // refreshes the techrequests data
+        location.reload()
+      },
+      (error: HttpErrorResponse) => {
+        this.getTechRequests();
+        console.log(error.message);
+      }
+    );
+  }
+
+  public deleteTechRequest(techrequestId: number): void {
+    this.techrequestService.deleteTechRequests(techrequestId).subscribe(
+      (response: string) => {
+        console.log(response);
+        this.getTechRequests(); // refreshes the techrequests data
+      },
+      (error: HttpErrorResponse) => {
+        this.getTechRequests();
+        console.log(error.message);
+      }
+    );
+  }
+
+  public onAddTechRequest(addForm: NgForm): void {
+    debugger;
+    //document.getElementById('add-techrequest-form').click();
+    this.techrequestService.addTechRequests(addForm.value).subscribe(
+      (response: string) => {
+        console.log(response);
+        this.getTechRequests();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+        this.getTechRequests();
+        addForm.reset();
+      }
+    );
+}
+
 }
--- autentica-frontend/src/app/app.module.ts
+++ autentica-frontend/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule } from '@angular/common/http'; import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
